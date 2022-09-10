8a2d53b7fa1992751afd7cf090da89d404524d8e
William Robert Aguilar Lancerio <99057287+WilliamAgLancerio@users.noreply.github.com>
2022-09-09 22:57:44 -0600

    CRUD
    SE IMPLEMENTO UN CRUD UTILIZANDO  ENTITY FRAMEWORK

---
+++ 20220907_ROBERT_AGUILAR/20220907_ROBERT_AGUILAR/Controllers/EVENTOController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _20220907_ROBERT_AGUILAR;
+
+namespace _20220907_ROBERT_AGUILAR.Controllers
+{
+    public class EVENTOController : Controller
+    {
+        private db_photoexpressEntities db = new db_photoexpressEntities();
+
+        // GET: EVENTO
+        public ActionResult Index()
+        {
+            return View(db.EVENTO.ToList());
+        }
+
+        // GET: EVENTO/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EVENTO eVENTO = db.EVENTO.Find(id);
+            if (eVENTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eVENTO);
+        }
+
+        // GET: EVENTO/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EVENTO/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_evento,nombre_institucion,direccion,cantidad_alumnos,hora_inicio,tipo_servicio,valor_servicio")] EVENTO eVENTO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EVENTO.Add(eVENTO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eVENTO);
+        }
+
+        // GET: EVENTO/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EVENTO eVENTO = db.EVENTO.Find(id);
+            if (eVENTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eVENTO);
+        }
+
+        // POST: EVENTO/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_evento,nombre_institucion,direccion,cantidad_alumnos,hora_inicio,tipo_servicio,valor_servicio")] EVENTO eVENTO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eVENTO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eVENTO);
+        }
+
+        // GET: EVENTO/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EVENTO eVENTO = db.EVENTO.Find(id);
+            if (eVENTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eVENTO);
+        }
+
+        // POST: EVENTO/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EVENTO eVENTO = db.EVENTO.Find(id);
+            db.EVENTO.Remove(eVENTO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
